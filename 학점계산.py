b87f88ac20c0f80bb1c1ec77e4f6932ad5bcba36
myeongseob91 <myeongseob91@naver.com>
2023-05-06 13:39:51 +0900

    학점계산

---
+++ 학점계산.py
@@ -0,0 +1,33 @@
+# Create an empty dictionary
+my_dict = {}
+
+# Add key-value pairs to the dictionary
+my_dict["A+"] = 4.3
+my_dict["A0"] = 4.0
+my_dict["A-"] = 3.7
+
+my_dict["B+"] = 3.3
+my_dict["B0"] = 3.0
+my_dict["B-"] = 2.7
+
+my_dict["C+"] = 2.3
+my_dict["C0"] = 2.0
+my_dict["C-"] = 1.7
+
+my_dict["D+"] = 1.3
+my_dict["D0"] = 1.0
+my_dict["D-"] = 0.7
+
+my_dict["F"] = 0.0
+
+credit = input()
+
+creditNumber = my_dict[credit]
+
+print(creditNumber)
+
+
+
+
+
+
