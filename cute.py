0de6e8fdfc5985babf21afad170e514c312ef82b
myeongseob91 <myeongseob91@naver.com>
2023-05-20 11:24:16 +0900

    git cute py

---
+++ cute.py
@@ -0,0 +1,12 @@
+
+loopCount = int(input())
+cuteCount = 0
+
+for i in range(loopCount):
+    cuteCount += int(input())
+
+if loopCount/2 < cuteCount:
+    print('Junhee is cute!')
+else:
+    print('Junhee is not cute!')
+
